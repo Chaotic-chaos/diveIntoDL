c2cc4a9c4b9d059b715f65ce245176ca71c66676
chaos_pc <life0531@foxmail.com>
2020-12-07 17:52:35 +0800

    2020-12-07

---
+++ chapter_3/chapter_3_dropout.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2020/12/7 下午1:35
+# @Author  : Chaos
+# @FileName: chapter_3_dropout.py
+# @Software: PyCharm
+# @Email   : life0531@foxmail.com
---
+++ chapter_4/chapter_4_buildModel.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2020/12/7 下午4:09
+# @Author  : Chaos
+# @FileName: chapter_4_buildModel.py
+# @Software: PyCharm
+# @Email   : life0531@foxmail.com
